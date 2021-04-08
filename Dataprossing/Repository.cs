ef356d3141a7a9319301a4b89395887b8edd324e
Fuan <fuanho@gmail.com>
2021-04-08 10:53:53 +0800

    Add asp.net web server

---
+++ Dataprossing/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Dataprossing
+{
+    public class Repository
+    {
+        public static List<PopulationAgeGroupInBaishaPenghu> readData(string filename)
+        {
+            string json = System.IO.File.ReadAllText(filename);
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new JsonStringEnumConverter());
+            var dataSet = JsonSerializer.
+                          Deserialize<List<PopulationAgeGroupInBaishaPenghu>>(json, options);
+
+            return dataSet;
+        }
+    }
+}
